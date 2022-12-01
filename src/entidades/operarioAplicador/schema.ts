4bea19b6c256674d03580ec99a543160c199074c
Fernando Cortese <cortesefernando@gmail.com>
2022-12-01 13:55:31 -0300

    UP

--- src/entidades/operarioAplicador/schema.ts
+++ src/entidades/operarioAplicador/schema.ts
@@ -1,6 +1,12 @@
+import { IEmpresaAplicadora } from "../empresaAplicadora";
+
 export interface IOperarioAplicador {
   _id?: string;
+  idEmpresaAplicadora?: string;
   email?: string;
   carnet?: string;
   vigenciaCarnet?: string;
+
+  // Populate
+  empresaAplicadora?: IEmpresaAplicadora;
 }
