b200b112a5fb27d5e40d8d4acc1b81a25283e54b
Fernando Cortese <cortesefernando@gmail.com>
2022-12-06 12:07:40 -0300

    create user

--- src/entidades/usuario/create.ts
+++ src/entidades/usuario/create.ts
@@ -1,5 +1,9 @@
+import { IDatosPersonales } from "./datosPersonales";
+
 export interface ICreateUsuario {
   email?: string;
   tokenVerificacion?: string;
   emailVerificado?: boolean;
+  fechaVerificacion?: string;
+  datosPersonales?: IDatosPersonales;
 }
