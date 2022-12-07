54f941d104b55677a22c70abf2129d4f75695a7e
Fernando Cortese <cortesefernando@gmail.com>
2022-12-07 16:05:15 -0300

    nivel en rol

--- src/entidades/rol/schema.ts
+++ src/entidades/rol/schema.ts
@@ -1,8 +1,10 @@
 import { IPerfil } from "../perfil";
+import { INivel } from "../rolUsuario";
 
 export interface IRol {
   _id?: string;
   nombre?: string;
+  nivel?: INivel;
   idsPerfiles?: string[];
   // Populate
   perfiles?: IPerfil[];
