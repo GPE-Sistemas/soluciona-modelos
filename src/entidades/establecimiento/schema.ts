610b10f748036d9b034a634057446645ad3afa44
Fernando Cortese <cortesefernando@gmail.com>
2022-12-07 15:51:26 -0300

    muni en establecimiento

--- src/entidades/establecimiento/schema.ts
+++ src/entidades/establecimiento/schema.ts
@@ -1,13 +1,16 @@
 import { IUbicacion } from "../../compartidos";
 import { IEmpresaProductora } from "../empresaProductora";
+import { IMunicipio } from "../municipio";
 
 export interface IEstablecimiento {
   _id?: string;
   nombre?: string;
   idEmpresaProductora?: string;
+  idMunicipio?: string;
   partidaInmobiliaria?: string;
   propio?: boolean;
   ubicacion: IUbicacion;
   // Populate
   empresaProductora?: IEmpresaProductora;
+  municipio?: IMunicipio;
 }
