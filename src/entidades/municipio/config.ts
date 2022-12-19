705577b37535cc86245c71e758b4133e4b1d0007
Fernando Cortese <cortesefernando@gmail.com>
2022-12-19 11:53:03 -0300

    config muni

--- src/entidades/municipio/config.ts
+++ src/entidades/municipio/config.ts
@@ -1,4 +1,7 @@
 export interface IConfigMunicipio {
+  solicitudRequiereAprobacionProductor?: boolean;
+  solicitudRequiereAprobacionAplicador?: boolean;
+  solicitudRequiereAprobacionIngeniero?: boolean;
   actaRequiereAprobacionProductor?: boolean;
   actaRequiereAprobacionAplicador?: boolean;
   actaRequiereAprobacionIngeniero?: boolean;
